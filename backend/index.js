7c2c7800e8dcf50cb066d9aea55550aacb2cbd53
dev21sept <dpatidar200214@gmail.com>
2026-03-09 19:09:39 +0530

    Configure backend to support Aiven MySQL with SSL and explicit config loading

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const productRoutes = require('./routes/productRoutes');
 
